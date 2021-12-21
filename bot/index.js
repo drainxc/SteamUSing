95aeddcd49a6bec84d31b9abd008b04c66e928ad
eastcopper <ldh7228@gmail.com>
2021-12-21 15:23:17 +0900

    ⚡ :: [Bot] Update Search

--- bot/index.js
+++ bot/index.js
@@ -27,6 +27,36 @@ client.on("message", (msg) => {
       }
     });
   }
+
+  if (msg.content.substring(0, 4) === "!노래 ") {
+    steam.getAppList().then((item) => {
+      console.log(item);
+      const sound = [
+        " - soundtrack",
+        " soundtrack",
+        " - original soundtrack",
+        " original soundtrack",
+        " - official soundtrack",
+        " official soundtrack",
+      ];
+      const soundtrack = sound.map(function (item) {
+        return (msg.content.slice(4) + item).toUpperCase();
+      });
+      for (let i = 0; i < item.length; i++) {
+        for (let j = 0; j < soundtrack.length; j++) {
+          if (item[i].name.toUpperCase() === soundtrack[j]) {
+            console.log(item[i].appid);
+            steam.getGameDetails(`${item[i].appid}`, false).then((object) => {
+              console.log(object);
+              if (object.type === "music") {
+                // msg.reply(object.name);
+              }
+            });
+          }
+        }
+      }
+    });
+  }
 });
 
 client.login("OTIyNDIxNDYzNDMxMTI3MDQx.YcBN7g.onI3iB1vI2NAJI4RA5b0RGpkH8U");
