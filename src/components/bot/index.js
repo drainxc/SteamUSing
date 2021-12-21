857a96b4aac8fe82412b720e932b394e9234b011
eastcopper <ldh7228@gmail.com>
2021-12-21 17:11:04 +0900

    ⚡ :: [Bot] Update contents

--- src/components/bot/index.js
+++ src/components/bot/index.js
@@ -2,6 +2,7 @@ const { Client, Intents } = require("discord.js");
 const client = new Client({
   intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
 });
+const { MessageEmbed } = require("discord.js");
 const API_KEY = "B89C41C6F58FEFB1FB51E4D5D73109A9";
 const SteamAPI = require("steamapi");
 const steam = new SteamAPI(API_KEY);
@@ -18,9 +19,46 @@ client.on("message", (msg) => {
         if (item[i].name.toUpperCase() === msg.content.slice(4).toUpperCase()) {
           console.log(item[i].appid);
           steam.getGameDetails(`${item[i].appid}`, false).then((object) => {
-            console.log(object);
             if (object.type === "game") {
-              // msg.reply(object.name);
+              console.log(object);
+              const infoEmbed = new MessageEmbed()
+                .setColor("#0099ff")
+                .setTitle(object.name)
+                .setURL(object.website)
+                .setAuthor(
+                  "SteamUSing",
+                  "https://i.imgur.com/AfFp7pu.png",
+                  object.website
+                )
+                .setDescription(`${object.short_description}`)
+                .setThumbnail(object.movies[0].thumbnail)
+                .addField(`${object.developers}`, `${object.support_info.email}`)
+                .addField("\u200B", "\u200B")
+                .addFields(
+                  {
+                    name: "현재 가격",
+                    value: `${object.price_overview.final_formatted }`,
+                    inline: true,
+                  },
+                  {
+                    name: "할인율",
+                    value: `${object.price_overview.discount_percent}` + "%",
+                    inline: true,
+                  },
+                  {
+                    name: "메타 스코어",
+                    value: `${object.metacritic.score}` + "점",
+                    inline: true,
+                  }
+                )
+                .setImage(object.header_image)
+                .setTimestamp()
+                .setFooter(
+                  "© " + `${object.publishers}`,
+                  "https://i.imgur.com/AfFp7pu.png"
+                );
+
+              msg.channel.send({ embeds: [infoEmbed] });
             }
           });
         }
