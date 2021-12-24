e8a6feace62c9b49ed2e2a77324edcc529c0396b
eastcopper <ldh7228@gmail.com>
2021-12-24 09:33:29 +0900

    📄 :: [Embed] Add infoEmbed

---
+++ src/lib/function/infoEmbed/index.js
@@ -0,0 +1,51 @@
+const { MessageEmbed } = require("discord.js");
+const color = "#0D7EAD";
+const logo =
+  "https://cdn.discordapp.com/attachments/921024184694497341/923239613617807371/Group_29.png";
+
+module.exports = {
+  infoEmbed: function (object) {
+    const {
+      name = "",
+      website = "",
+      short_description = "",
+      header_image = "",
+      developers = "",
+      price_overview = "",
+      recommendations = "",
+      publishers = "",
+    } = object;
+    const { final_formatted = "무료", discount_percent = "없음" } =
+      price_overview;
+    const { total = "없음" } = recommendations;
+    const infoEmbed = new MessageEmbed()
+      .setColor(color)
+      .setTitle(name)
+      .setURL(website)
+      .setAuthor("SteamUSing", logo, website)
+      .setDescription(`${short_description}`)
+      .addField("개발사", `${developers[0]}`)
+      .addField("\u200B", "\u200B")
+      .addFields(
+        {
+          name: "현재 가격",
+          value: `${final_formatted}`,
+          inline: true,
+        },
+        {
+          name: "할인율",
+          value: `${discount_percent}` + "%",
+          inline: true,
+        },
+        {
+          name: "평가 갯수",
+          value: `${total}`,
+          inline: true,
+        }
+      )
+      .setImage(header_image)
+      .setTimestamp()
+      .setFooter(`${publishers[0]}`, logo);
+    return infoEmbed;
+  }
+};
